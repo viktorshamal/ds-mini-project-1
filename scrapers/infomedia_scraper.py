9a7c1f331eaa4c714414b3d8f22dfea3d83492ea
Viktor Shamal Andersen <sphinxominator@gmail.com>
2018-02-22 14:12:45 +0100

    Added a script that converts Infomedia searches to a list of ArticleIds

---
+++ scrapers/infomedia_scraper.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import pprint
+import glob
+import os
+
+pp = pprint.PrettyPrinter()
+
+
+def article_file_to_ids(directory):
+    path = '../infomedia_data/'
+    article_files = glob.glob(path + "*.json")
+
+    for af in article_files:
+        path = os.path.splitext(af)[0]
+        articles = json.load(open(af, errors="replace"))['Articles']
+
+        with open(path + '_ids_.txt', 'w') as f:
+            for article in articles:
+                f.write(article['Duid'] + '\n')
+
+
+def get_article(id):
+    url = "https://apps.infomedia.dk/mediearkiv/article/getarticlebodytext"
+
+    payload = "{\r\n   \"ArticleId\":\"" + id + \
+        "\",\r\n   \"BasicSearchFilter\":{\r\n      \"SearchClause\":{\r\n         \"AvailableSearchLocations\":[\r\n            0,\r\n            1,\r\n            2,\r\n            3\r\n         ]\r\n      }\r\n  }\r\n}"
+    headers = {
+        'Host': "apps.infomedia.dk",
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0",
+        'Cookie': "Insight.Web.Ms.SessionId=nmn3duoyxcejsvgvj0iq4hef",
+        'Content-Type': "application/json",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "6d23f872-bf1b-0247-912b-cc9ac2078068"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    data = json.loads(response.text)
+
+    return data
+
+
+article_file_to_ids('vesterbro')
